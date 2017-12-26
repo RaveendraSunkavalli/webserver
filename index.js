fdfd249bcaf08509e9cc0de77e497171a0e34143
Ravi <s.ravindra153@gmail.com>
2017-12-26 16:16:17 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
-
+const port=process.env.PORT||3000;
 var app=express();
 hbs.registerPartials(__dirname+'/views/partials');
 app.set("view engine",'hbs');
@@ -65,4 +65,6 @@ app.get('/bad',(req,res)=>{
 
 });
 
-app.listen(3000);
+app.listen(port,()=>{
+	console.log(port)
+});
